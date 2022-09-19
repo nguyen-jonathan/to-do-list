9875087ea8848a1094e0c6976f65af2b01b73827
Jonathan Nguyen <njonathan990@gmail.com>
2022-09-18 23:59:04 -0500

    hard coded tasks in toDoList

--- src/components/ToDoList/ToDoList.jsx
+++ src/components/ToDoList/ToDoList.jsx
@@ -3,15 +3,23 @@ import ToDoItem from '../ToDoItem/ToDoItem';
 import {useSelector, useDispatch} from 'react-redux';
 
 const ToDoList = () => {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
   // from reducer/store
-  const todos = useSelector((state) => state.todos);
+  const todos = [
+    {id: 1, title: 'todo1', completed: false},
+    {id: 2, title: 'todo2', completed: false},
+    {id: 3, title: 'todo3', completed: true},
+  ];
+
+  useSelector((state) => state.todos);
 
   // to list out to do tasks
   // map out todo from store
   return (
-    <ul>
-      <ToDoItem />
+    <ul className="list-group">
+      {todos.map((todo) => (
+        <ToDoItem id={todo.id} title={todo.title} completed={todo.completed} />
+      ))}
     </ul>
   );
 };
