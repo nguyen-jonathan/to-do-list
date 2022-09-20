b3a896c78eaf7c3ff99bad61b14095a69aa70203
Jonathan Nguyen <njonathan990@gmail.com>
2022-09-20 00:25:48 -0500

    added completeItems for total- placeholder

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AddToDoForm from '../AddToDoForm/AddToDoForm';
 import ToDoList from '../ToDoList/ToDoList';
+import TotalCompleteItems from '../TotalCompleteItems/TotalCompleteItems';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <h1>ToDo List</h1>
       <AddToDoForm />
       <ToDoList />
+      <TotalCompleteItems />
     </div>
   );
 };
